989451194639cfd7e8114f62281e3833105d1753
SoumenCodes <soumen19j@gmail.com>
2023-04-29 22:55:27 +0530

    Updates

---
+++ precache-manifest.c69c1d84b71c0a69c9ad14cc3cb47077.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "a85416f327917c70fda9",
+    "url": "/Text-Analyzer/static/css/main.7f0b9c7d.chunk.css"
+  },
+  {
+    "revision": "a85416f327917c70fda9",
+    "url": "/Text-Analyzer/static/js/main.a85416f3.chunk.js"
+  },
+  {
+    "revision": "7fc5741dfb5d402e95fa",
+    "url": "/Text-Analyzer/static/js/1.7fc5741d.chunk.js"
+  },
+  {
+    "revision": "555471da2a8b845fc08c",
+    "url": "/Text-Analyzer/static/js/2.555471da.chunk.js"
+  },
+  {
+    "revision": "7fb2a9e5828552fda9a8",
+    "url": "/Text-Analyzer/static/js/runtime~main.7fb2a9e5.js"
+  },
+  {
+    "revision": "06e733283fa43d1dd57738cfc409adbd",
+    "url": "/Text-Analyzer/static/media/logo.06e73328.svg"
+  },
+  {
+    "revision": "5ba01f4ac218831dcc80b22792ce6d06",
+    "url": "/Text-Analyzer/index.html"
+  }
+];
